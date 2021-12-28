ac3957fb2d59e33d6a8ac811fcc527979173c8c8
AJ Rivera <aj@compumed.tech>
2021-12-28 16:05:52 -0500

    start project using xcode's built-in project starter

---
+++ resume/resume/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  resume
+//
+//  Created by AJ Rivera on 12/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
